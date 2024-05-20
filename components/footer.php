f2d34f8edee7c76167d66a84d47b6edb627ce68b
KaziMoyan <moyan35-3156@diu.edu.bd>
2024-05-20 23:44:11 +0600

    first commit

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:moyeenkazi03@gmail.com">moyeenkazi03@gmail.com</a>
+         <a href="mailto:moyeenkazi03@gmailcom">moyeenkazi03@gmailcom</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Dhanmodi,32-A,road-8</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">01831435150</a>
+         <a href="tel:1112223333">01313894552</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Moyeen kazi
+   </span> Wish Good day for you</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
